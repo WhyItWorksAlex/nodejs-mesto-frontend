26a2ea0ab49222cbbcacb9514cac4f104bd4f62b
WhyItWorksAlex <fishman1q2w3e@gmail.com>
2025-01-24 20:01:49 +0200

    deploy

--- src/utils/api.js
+++ src/utils/api.js
@@ -130,6 +130,6 @@ class Api {
   }
 }
 // Замените на адрес вашего бэкенда
-const api = new Api("http://localhost:3000");
+const api = new Api("https://api.fishmanalex.students.nomorepartiesco.ru/");
 
 export default api;
